ed92bf00def1997c9e150631c363a1aaa426e5ab
anastacyaemanratu <anastacyaemanratu@gmail.com>
2026-06-11 22:52:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,62 @@
-function openLetter(n,m){overlay.style.display='flex';t.innerText='💌 Untuk '+n;c.innerText=m;}
-function closeLetter(){overlay.style.display='none';}
-```javascript
-function openImage(src){
+const stars=document.getElementById('stars');
 
-document.getElementById("lightbox").style.display="flex";
-document.getElementById("lightbox-img").src=src;
-document.getElementById("downloadBtn").href=src;
-}
-function closeImage(){
-document.getElementById("lightbox").style.display="none";
+/* BINTANG */
+
+for(let i=0;i<600;i++){
+
+const star=document.createElement('div');
+
+star.className='star';
+
+const size=Math.random()*3+1;
+
+star.style.width=size+'px';
+star.style.height=size+'px';
+
+star.style.left=Math.random()*100+'%';
+star.style.top=Math.random()*100+'%';
+
+star.style.animationDelay=Math.random()*3+'s';
+
+stars.appendChild(star);
 
 }
-```
+
+/* BINTANG JATUH */
+
+function createShootingStar(){
+
+const star=document.createElement('div');
+
+star.className='shooting-star';
+
+star.style.width=(Math.random()*250+100)+'px';
+
+star.style.top=(Math.random()*40)+'%';
+
+star.style.left='-300px';
+
+document.body.appendChild(star);
+
+star.animate(
+[
+{
+transform:'translate(0,0) rotate(25deg)'
+},
+{
+transform:'translate(2000px,900px) rotate(25deg)'
+}
+],
+{
+duration:2500,
+easing:'linear'
+}
+);
+
+setTimeout(()=>{
+star.remove();
+},2500);
+
+}
+
+setInterval(createShootingStar,1000);
