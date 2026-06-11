58b6fd772019df2848e89ad437365d215b235801
anastacyaemanratu <anastacyaemanratu@gmail.com>
2026-06-11 13:30:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,10 @@ function closeLetter(){overlay.style.display='none';}
 function openImage(src){
 
 document.getElementById("lightbox").style.display="flex";
-
 document.getElementById("lightbox-img").src=src;
-
 document.getElementById("downloadBtn").href=src;
-
 }
-
 function closeImage(){
-
 document.getElementById("lightbox").style.display="none";
 
 }
