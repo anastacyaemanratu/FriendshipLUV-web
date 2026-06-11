5efb778d63d68d6c5437c92de469d4f7721d0ba7
anastacyaemanratu <anastacyaemanratu@gmail.com>
2026-06-11 13:30:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 function openLetter(n,m){overlay.style.display='flex';t.innerText='💌 Untuk '+n;c.innerText=m;}
 function closeLetter(){overlay.style.display='none';}
+```javascript
+function openImage(src){
+
+document.getElementById("lightbox").style.display="flex";
+
+document.getElementById("lightbox-img").src=src;
+
+document.getElementById("downloadBtn").href=src;
+
+}
+
+function closeImage(){
+
+document.getElementById("lightbox").style.display="none";
+
+}
+```
